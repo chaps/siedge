f8fc81056951b590dac3eb2c099ed46fd685a07f
Antonio Herran <antonioherran@gmail.com>
2017-11-07 00:53:58 -0600

    angular stuff + multilanguage

--- app/siedge.js
+++ app/siedge.js
@@ -1,2 +1,31 @@
-angular
-    .module("siedgeModule", ['ngMaterial', 'ui.bootstrap']);
+var app = angular.module("siedgeApp", ['pascalprecht.translate']);
+
+app.controller("siedgeController", function($scope, $translate){
+    $scope.languages = [
+        { language: 'es', },
+        { language: 'en' }
+    ];
+
+    $scope.selectedlanguage = $scope.languages[1];
+
+    $scope.changeLanguage = function (langKey) {
+        $translate.use(langKey);
+    };
+});
+
+app.config(function ($translateProvider) {
+    $translateProvider.translations('es', {
+        ABOUT: 'inteligencia en ingenieria',
+        LANG: 'Idioma',
+        LANG1: 'Espanol',
+        LANG2: 'Ingles'
+    });
+    $translateProvider.translations('en', {
+        ABOUT: 'Engineering Intelligence',
+        LANG: 'Language',
+        LANG1: 'Spanish',
+        LANG2: 'English'
+    })
+    $translateProvider.preferredLanguage('es');
+    $translateProvider.useSanitizeValueStrategy('escapeParameters');
+});
